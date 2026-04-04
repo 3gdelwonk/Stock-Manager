bb711e36c9d11700da8254cd6017543ad74caf4e
3gdelwonk <3gdelwonk@users.noreply.github.com>
2026-04-04 02:55:34 +0000

    deploy: 147940cdb64b6f703be9d87c0fbcaed77639d8b0

--- assets/zap-BJcyvlOr.js
+++ assets/zap-BJcyvlOr.js
@@ -1,4 +1,4 @@
-import{c as a}from"./BarcodeScanner-DGWzu6CE.js";/**
+import{c as a}from"./BarcodeScanner-DpStldCa.js";/**
  * @license lucide-react v0.479.0 - ISC
  *
  * This source code is licensed under the ISC license.
