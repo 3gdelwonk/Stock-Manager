e78f79f7c42f09a5712391ea56c17a375ef8e58f
3gdelwonk <3gdelwonk@users.noreply.github.com>
2026-04-04 13:29:53 +0000

    deploy: ffb42b43c976341248fa9f8e39a01398a321d998

--- assets/zap-Q5pNMerl.js
+++ assets/zap-Q5pNMerl.js
@@ -1,4 +1,4 @@
-import{c as a}from"./BarcodeScanner-D-GtuIrp.js";/**
+import{c as a}from"./BarcodeScanner-CBIQ-sEK.js";/**
  * @license lucide-react v0.479.0 - ISC
  *
  * This source code is licensed under the ISC license.
