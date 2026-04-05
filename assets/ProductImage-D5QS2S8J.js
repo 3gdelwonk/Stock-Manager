d6fd08b40f6c5efdd249004bab7459dec9d51a98
3gdelwonk <3gdelwonk@users.noreply.github.com>
2026-04-05 07:22:14 +0000

    deploy: c537e8779dbca38cf1cae71f999d042e97ce89d3

--- assets/ProductImage-D5QS2S8J.js
+++ assets/ProductImage-D5QS2S8J.js
@@ -1,4 +1,4 @@
-import{c as h,d as l,r as ne,j as s,R as D,H as Y,S as F,X as ce}from"./BarcodeScanner-dmiXsCot.js";import{r as f}from"./vendor-g3Nuhkd9.js";/**
+import{c as h,d as l,r as ne,j as s,R as D,H as Y,S as F,X as ce}from"./BarcodeScanner-BGYLGlcS.js";import{r as f}from"./vendor-g3Nuhkd9.js";/**
  * @license lucide-react v0.479.0 - ISC
  *
  * This source code is licensed under the ISC license.
